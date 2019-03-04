ac068219342ce6b5a89a437b95e04ec07a3f9548
IsaiasLeos <blade_master_pantherinae@hotmail.com>
2019-03-04 16:33:39 +0000

    merged product.java from previous project + implementation into current project

--- pricewatcher/src/pricewatcher/Main.java
+++ pricewatcher/src/pricewatcher/Main.java
@@ -6,7 +6,7 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
-
+import java.util.ArrayList;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -15,105 +15,134 @@ import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 /**
-* A dialog for tracking the price of an item.
-*
-* @author Yoonsik Cheon
-*/
+ * A dialog for tracking the price of an item.
+ *
+ * @author Yoonsik Cheon
+ */
 @SuppressWarnings("serial")
 public class Main extends JFrame {
 
-    /** Default dimension of the dialog. */
+    private Product product;
+    private java.util.List<Product> productList;
+
+    /**
+     * Default dimension of the dialog.
+     */
     private final static Dimension DEFAULT_SIZE = new Dimension(400, 300);
-      
-    /** Special panel to display the watched item. */
+
+    /**
+     * Special panel to display the watched item.
+     */
     private ItemView itemView;
-      
-    /** Message bar to display various messages. */
+
+    /**
+     * Message bar to display various messages.
+     */
     private JLabel msgBar = new JLabel(" ");
 
-    /** Create a new dialog. */
+    /**
+     * Create a new dialog.
+     */
     public Main() {
-    	this(DEFAULT_SIZE);
+        this(DEFAULT_SIZE);
     }
-    
-    /** Create a new dialog of the given screen dimension. */
+
+    /**
+     * Create a new dialog of the given screen dimension.
+     *
+     * @param dim
+     */
     public Main(Dimension dim) {
         super("Price Watcher");
+        String itemURL = "https://www.amazon.com/Nintendo-Console-Resolution-Surround-Customize/dp/B07M5ZQSKV";
+        String itemName = "Nintendo Switch";
+        double itemPrice = 359.99;
+        String itemDateAdded = "1/30/2019";
+        this.product = new Product(itemName, itemURL, itemPrice, itemDateAdded);
+        this.productList = new ArrayList<>();
+        productList.add(product);
         setSize(dim);
-        
         configureUI();
-        //setLocationRelativeTo(null);
+        setLocationRelativeTo(null);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setVisible(true);
-        //setResizable(false);
+        setResizable(true);
         showMessage("Welcome!");
     }
-  
-    /** Callback to be invoked when the refresh button is clicked. 
-     * Find the current price of the watched item and display it 
-     * along with a percentage price change. */
+
+    /**
+     * Callback to be invoked when the refresh button is clicked. Find the
+     * current price of the watched item and display it along with a percentage
+     * price change.
+     */
     private void refreshButtonClicked(ActionEvent event) {
-    	//--
-    	//-- WRITE YOUR CODE HERE!
-    	//--
-    	showMessage("Refresh clicked!");
+
     }
-    
-    /** Callback to be invoked when the view-page icon is clicked.
-     * Launch a (default) web browser by supplying the URL of
-     * the item. */
-    private void viewPageClicked() {    	
-    	//--
-    	//-- WRITE YOUR CODE HERE!
-    	//--
-    	showMessage("View clicked!");
+
+    /**
+     * Callback to be invoked when the view-page icon is clicked. Launch a
+     * (default) web browser by supplying the URL of the item.
+     */
+    private void viewPageClicked() {
+
     }
-        
-    /** Configure UI. */
+
+    /**
+     * Configure UI.
+     */
     private void configureUI() {
         setLayout(new BorderLayout());
         JPanel control = makeControlPanel();
-        control.setBorder(BorderFactory.createEmptyBorder(10,16,0,16)); 
+        control.setBorder(BorderFactory.createEmptyBorder(10, 16, 0, 16));
         add(control, BorderLayout.NORTH);
         JPanel board = new JPanel();
         board.setBorder(BorderFactory.createCompoundBorder(
-        		BorderFactory.createEmptyBorder(10,16,0,16),
-        		BorderFactory.createLineBorder(Color.GRAY)));
-        board.setLayout(new GridLayout(1,1));
+                BorderFactory.createEmptyBorder(10, 16, 0, 16),
+                BorderFactory.createLineBorder(Color.GRAY)));
+        board.setLayout(new GridLayout(1, 1));
         itemView = new ItemView();
         itemView.setClickListener(this::viewPageClicked);
         board.add(itemView);
         add(board, BorderLayout.CENTER);
-        msgBar.setBorder(BorderFactory.createEmptyBorder(10,16,10,0));
+        msgBar.setBorder(BorderFactory.createEmptyBorder(10, 16, 10, 0));
         add(msgBar, BorderLayout.SOUTH);
     }
-      
-    /** Create a control panel consisting of a refresh button. */
+
+    /**
+     * Create a control panel consisting of a refresh button.
+     */
     private JPanel makeControlPanel() {
-    	JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEADING));
-    	JButton refreshButton = new JButton("Refresh");
-    	refreshButton.setFocusPainted(false);
+        JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEADING));
+        JButton refreshButton = new JButton("Refresh");
+        refreshButton.setFocusPainted(false);
         refreshButton.addActionListener(this::refreshButtonClicked);
         panel.add(refreshButton);
         return panel;
     }
 
-    /** Show briefly the given string in the message bar. */
+    /**
+     * Show briefly the given string in the message bar.
+     */
     private void showMessage(String msg) {
         msgBar.setText(msg);
         new Thread(() -> {
-        	try {
-				Thread.sleep(3 * 1000); // 3 seconds
-			} catch (InterruptedException e) {
-			}
-        	if (msg.equals(msgBar.getText())) {
-        		SwingUtilities.invokeLater(() -> msgBar.setText(" "));
-        	}
+            try {
+                Thread.sleep(3 * 1000); // 3 seconds
+            } catch (InterruptedException e) {
+            }
+            if (msg.equals(msgBar.getText())) {
+                SwingUtilities.invokeLater(() -> msgBar.setText(" "));
+            }
         }).start();
     }
-    
+
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
-        new Main();
+        Main main = new Main();
+
     }
 
 }
---
+++ pricewatcher/src/pricewatcher/Product.java
@@ -0,0 +1,170 @@
+package pricewatcher;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+/**
+ * This class holds information about the current product and changes done to
+ * it.
+ *
+ * @author Isaias Leos
+ */
+public class Product {
+
+    private String productURL;
+    private String productName;
+    private String addedDate;
+    private double productPrice;
+    private double change;
+    private double initialPrice;
+
+    /**
+     * Alternative constructor for the product with given information.
+     *
+     * @param productName name of product
+     * @param currentURL current URL of the product
+     * @param initialPrice price when first added
+     * @param addedDate date product was added
+     */
+    public Product(String productName, String currentURL, double initialPrice, String addedDate) {
+        this.productURL = currentURL;
+        this.productName = productName;
+        this.initialPrice = initialPrice;
+        this.productPrice = initialPrice;
+        this.addedDate = addedDate;
+    }
+
+    /**
+     * Default constructor for the product.
+     */
+    public Product() {
+    }
+
+    /**
+     * Sets the price to a new calculated price and updates the change.
+     *
+     * @param newPrice
+     */
+    public void checkPrice(double newPrice) {
+        setProductPrice(newPrice);
+        setChange(new BigDecimal(calculateProductChange(getInitialPrice(),
+                getProductPrice())).setScale(2, RoundingMode.CEILING).doubleValue());
+    }
+
+    /**
+     * This method computes the percentage difference between two numbers.
+     *
+     * @param initialPrice initial price of the product
+     * @param newPrice the current price of the product
+     * @return the difference shown as a percentage
+     */
+    private double calculateProductChange(double newPrice, double initialPrice) {
+        return ((newPrice - initialPrice) / initialPrice) * 100;
+    }
+
+    /**
+     *
+     * @return returns the current URL of the item being watched.
+     */
+    public String getProductURL() {
+        return productURL;
+    }
+
+    /**
+     * Replaces the current URL of the item being watched.
+     *
+     * @param productURL the name of the URL that will be replacing the current
+     * item name.
+     */
+    public void setCurrentURL(String productURL) {
+        this.productURL = productURL;
+    }
+
+    /**
+     *
+     * @return returns the name of the current item.
+     */
+    public String getProductName() {
+        return productName;
+    }
+
+    /**
+     * Replaces the current Name of the item being watched.
+     *
+     * @param productName the name of the item that will be replacing the
+     * current item.
+     */
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    /**
+     *
+     * @return returns the price of the current item.
+     */
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    /**
+     * Replaces the current Price of the item being watched.
+     *
+     * @param productPrice the price that will be replacing the current price of
+     * the item.
+     */
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    /**
+     *
+     * @return the current date of when the product was added
+     */
+    public String getAddedDate() {
+        return addedDate;
+    }
+
+    /**
+     * Sets the date of when a product was added.
+     *
+     * @param addedDate
+     */
+    public void setAddedDate(String addedDate) {
+        this.addedDate = addedDate;
+    }
+
+    /**
+     *
+     * @return the value of change between the initial price and the new price.
+     */
+    public double getChange() {
+        return change;
+    }
+
+    /**
+     * Sets the value of difference between two prices.
+     *
+     * @param change
+     */
+    public void setChange(double change) {
+        this.change = change;
+    }
+
+    /**
+     * Get the initial price of a product.
+     *
+     * @return
+     */
+    public double getInitialPrice() {
+        return initialPrice;
+    }
+
+    /**
+     * Sets the initial price of an item, if wrong.
+     *
+     * @param initialPrice
+     */
+    public void setInitialPrice(double initialPrice) {
+        this.initialPrice = initialPrice;
+    }
+}
